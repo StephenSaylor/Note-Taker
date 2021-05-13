f057ddaf93894390ad404fb7c2cfb80678ccb7e0
Jeff Lebowski <anentitymusic@gmail.com>
2021-05-13 17:23:39 -0400

    begin routes

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -8,3 +8,6 @@ const PORT = process.env.PORT || 6000
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')))
