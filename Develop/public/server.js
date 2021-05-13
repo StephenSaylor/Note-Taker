b38c799f44b5816051e248d32e01fbc589f236f4
Jeff Lebowski <anentitymusic@gmail.com>
2021-05-13 17:13:20 -0400

    dependecies app port and express

---
+++ Develop/public/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+
+const app = express()
+const PORT = process.env.PORT || 6000
+
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
+
